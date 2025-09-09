da942d3162ef531aef50ea3110ac9ae44201902f
Peter Pan <apppeterpan@gmail.com>
2025-09-09 11:23:44 +0800

    Initial Commit

---
+++ MakePlayfulDesignsWithShapesSwiftUI206/MakePlayfulDesignsWithShapesSwiftUI206App.swift
@@ -0,0 +1,17 @@
+//
+//  MakePlayfulDesignsWithShapesSwiftUI206App.swift
+//  MakePlayfulDesignsWithShapesSwiftUI206
+//
+//  Created by SHIH-YING PAN on 2025/9/9.
+//
+
+import SwiftUI
+
+@main
+struct MakePlayfulDesignsWithShapesSwiftUI206App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
